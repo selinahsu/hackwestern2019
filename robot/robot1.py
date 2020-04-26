52a09eb628837fae8be3e5f9beb364659192b3f5
selinahsu <selinaraindrop@gmail.com>
2020-04-26 15:59:01 -0400

    before deleting locally

--- robot/robot1.py
+++ robot/robot1.py
@@ -1,24 +1,46 @@
 import RPi.GPIO as GPIO
 import time
 
+# robby = Robot(left=(7,8), right=(9,10))
+
 # Use pin numbers instead of GPIO numbering
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(40, GPIO.OUT) # pin 40 activates H bridge
-GPIO.setup(38, GPIO.IN) # pin 38 awaits button input
+In1 = 40
+In2 = 37
+In3 = 35
+In4 = 33
+button = 38
 
-while GPIO.input(38) == GPIO.LOW:  # wait for button press
-    time.sleep(1)
+# these pins activate the H bridge
+GPIO.setup(In1, GPIO.OUT)
+GPIO.setup(In2, GPIO.OUT)
+GPIO.setup(In3, GPIO.OUT)
+GPIO.setup(In4, GPIO.OUT)
 
-# exits while loop when pin 38 receives input
-print("Button was pushed!")
+# this pin is a button
+GPIO.setup(button, GPIO.IN)
 
-time.sleep(3)
+while True: #stay in this loop forever
+    while GPIO.input(button) == GPIO.LOW:  # waiting for button input
+        time.sleep(1)
 
-for x in range(3):
-    GPIO.output(40, GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(40, GPIO.LOW)
-    time.sleep(3)
+    # exits while loop when pin 38 receives input
+    print("Button is pushed!")
+
+    for x in "xx":
+        time.sleep(4)
+        GPIO.output(In2, GPIO.HIGH)
+        GPIO.output(In4, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(In2, GPIO.LOW)
+        GPIO.output(In4, GPIO.LOW)
+
+    time.sleep(4)
+    GPIO.output(In1, GPIO.HIGH)
+    GPIO.output(In3, GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(In1, GPIO.LOW)
+    GPIO.output(In3, GPIO.LOW)
 
 GPIO.cleanup()
